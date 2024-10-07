e7f54cf438bb192b9454760b9488667a3b1c49ea
tbruhnuncc <tbruhn@uncc.edu>
2024-10-07 18:22:29 -0400

    Initial Commit

---
+++ Flashcard Study App/Flashcard_Study_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashcard_Study_AppApp.swift
+//  Flashcard Study App
+//
+//  Created by Thomas Bruhn on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Flashcard_Study_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Flashcard Study AppTests/Flashcard_Study_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Flashcard_Study_AppTests.swift
+//  Flashcard Study AppTests
+//
+//  Created by Thomas Bruhn on 10/7/24.
+//
+
+import Testing
+@testable import Flashcard_Study_App
+
+struct Flashcard_Study_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
